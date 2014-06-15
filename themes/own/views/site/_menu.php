6c434e07050cf582829eb03e6ca14218e271b77f
Harutyun Margaryan <harutyun_margaryan@edu.aua.am>
2014-06-15 22:09:46 +0400

    save 6.15.2014  10:09 pm

--- themes/own/views/site/_menu.php
+++ themes/own/views/site/_menu.php
@@ -5,3 +5,26 @@
 ?>
 
 
+<div class="wrapper col2">
+    <div id="topbar1">
+        <div id="topnav">
+            <ul>
+                <li class="active"><a href="http://www.ssystems.am/home">Главное
+                    </a></li>
+                <li><a href="http://www.ssystems.am/products">Продукты
+                    </a></li>
+                <li><a href="http://www.ssystems.am/clients">Партнеры
+                    </a></li>
+                <li><a href="http://www.ssystems.am/certificates">Сертификаты
+                    </a></li>
+                <li><a href="http://www.ssystems.am/news">О НАС
+                    </a></li>
+                <li><a href="http://www.ssystems.am/gallery">Галерея
+                    </a></li>
+                <li><a href="http://www.ssystems.am/contacts">Контакты
+                    </a></li>
+            </ul>
+        </div>
+        <br class="clear">
+    </div>
+</div>
