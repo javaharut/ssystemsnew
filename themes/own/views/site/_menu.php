39b6bafaaa59a39938e262802d1891c1a5cd68db
Harutyun Margaryan <harutyun_margaryan@edu.aua.am>
2014-06-15 23:15:40 +0400

    save 6.15.2014  11:15 pm

--- themes/own/views/site/_menu.php
+++ themes/own/views/site/_menu.php
@@ -1,14 +1,26 @@
 <?php
 /* @var $this SiteController */
-
-
 ?>
-
 
 <div class="wrapper col2">
     <div id="topbar1">
         <div id="topnav">
-            <ul>
+            <?php
+            $this->widget('zii.widgets.CMenu', array(
+                'items'=>array(
+                    // Important: you need to specify url as 'controller/action',
+                    // not just as 'controller' even if default acion is used.
+                    array('label'=>'Home', 'url'=>array('site/index')),
+                    // 'Products' menu item will be selected no matter which tag parameter value is since it's not specified.
+                    array('label'=>'Products', 'url'=>array('product/index'), 'items'=>array(
+                        array('label'=>'New Arrivals', 'url'=>array('product/new', 'tag'=>'new')),
+                        array('label'=>'Most Popular', 'url'=>array('product/index', 'tag'=>'popular')),
+                    )),
+                    array('label'=>'Login', 'url'=>array('site/login'), 'visible'=>Yii::app()->user->isGuest),
+                ),
+            ));
+            ?>
+            <!--<ul>
                 <li class="active"><a href="http://www.ssystems.am/home">Главное
                     </a></li>
                 <li><a href="http://www.ssystems.am/products">Продукты
@@ -23,7 +35,7 @@
                     </a></li>
                 <li><a href="http://www.ssystems.am/contacts">Контакты
                     </a></li>
-            </ul>
+            </ul>-->
         </div>
         <br class="clear">
     </div>
